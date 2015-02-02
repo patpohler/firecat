371f6fcf745fc1798f691dc5f27bd4d94eb9610d
Andrew Clinton <ddrew.clinton@gmail.com>
2015-02-02 17:22:37 -0500

    added routes for password get and reset

--- src/routes.php
+++ src/routes.php
@@ -18,6 +18,26 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth'), function() {
 Route::get('/logout', array('as' => 'logout',
     'uses' => 'Anecka\Firecat\Controllers\SessionController@getLogout'));
 
+Route::get('password/reset', array(
+  'uses' => 'Anecka\Firecat\Controllers\PasswordController@remind',
+  'as' => 'password.remind'
+));
+
+Route::post('password/reset', array(
+  'uses' => 'Anecka\Firecat\Controllers\PasswordController@request',
+  'as' => 'password.request'
+));
+
+Route::get('password/reset/{token}', array(
+  'uses' => 'Anecka\Firecat\Controllers\PasswordController@reset',
+  'as' => 'password.reset'
+));
+
+Route::post('password/reset/{token}', array(
+  'uses' => 'Anecka\Firecat\Controllers\PasswordController@update',
+  'as' => 'password.update'
+));
+
 /*
 Route::get('/firecat', function() {
     //return View::make('Anecka\Firecat\Controllers\HomeController@showStart');
